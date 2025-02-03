6b479addebb3b9c97e57dadc7c3c53481bbd1c91
Kareem <kareem.abdalrazzaq@gmail.com>
2025-02-03 10:40:53 +0100

    feat: add authentication and user management routes, components, and configuration

---
+++ src/routes/auth/login.route.jsx
@@ -0,0 +1,12 @@
+import LoginForm from "../../components/login.form.compnent";
+
+const Login = () => {
+  return (
+    <div>
+      <h3>Login</h3>
+      <LoginForm />
+    </div>
+  );
+};
+
+export default Login;
